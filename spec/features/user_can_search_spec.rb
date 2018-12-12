5cf7bd5ed1e46efe8423fc7895f26fddb2cf0208
Meghan Stang <mstang15@gmail.com>
2018-12-12 09:24:52 -0700

    adds search feature test

---
+++ spec/features/user_can_search_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'as a user when I visit /' do
+  before(:each) do
+    # When I visit "/"
+    visit '/'
+    # And I fill in the search form with 80203 (Note: Use the existing search form)
+    fill_in :search, with: "80203"
+    # And I click "Locate"
+    click_on "Locate"
+
+    @stations = Search.last
+  end
+  it 'when I fill in search and click locate I am redirected to /search' do
+    # Then I should be on page "/search"
+    expect(current_path).to eq('/search')
+  end
+
+  it 'and search, I should see a list of the 10 closest stations within 6 miles sorted by distance' do
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(page).to have_content("Closest Stations")
+    # And the stations should be limited to Electric and Propane
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    within('.stations') do
+      @stations.each do |station|
+        expect(station.fuel_type).to eq("LPG" || "ELEC")
+        expect(station.name).to exist
+        expect(station.address).to exist
+        expect(station.fuel_type).to exist
+        expect(station.distance).to exist
+        expect(station.access_time).to exist
+      end
+    end
+  end
+end
